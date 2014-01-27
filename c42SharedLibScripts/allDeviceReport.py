e40c1da910a98d55ae78dbea923eccfca77cd9c5
grahamfw <graham.wells@gmail.com>
2014-01-27 14:16:49 -0500

    Update allDeviceReport.py
    Update to prompt for password and not require cleartext storage of credentials.

--- c42SharedLibScripts/allDeviceReport.py
+++ c42SharedLibScripts/allDeviceReport.py
@@ -18,12 +18,13 @@ import math
 from dateutil.relativedelta import *
 import datetime
 import calendar
+import getpass
 
 
 c42Lib.cp_host = "https://10.10.10.10"
 c42Lib.cp_port = "4285"
 c42Lib.cp_username = "admin"
-c42Lib.cp_password = "admin"
+c42Lib.cp_password = getpass.getpass('Enter your CrashPlan console password: ')
 
 c42Lib.cp_logLevel = "INFO"
 c42Lib.cp_logFileName = "allDeviceReport.log"
